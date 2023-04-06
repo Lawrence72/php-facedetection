3512f64dbf5a715ed13cdfa509a0e7965cbccfdd
Mr Percival <49914839+Lawrence72@users.noreply.github.com>
2023-04-06 06:59:59 -0500

    Update FaceDetector.php
    updates for PHP 8.2

--- FaceDetector.php
+++ FaceDetector.php
@@ -19,13 +19,11 @@
 // @Contributor Maurice Svay
 //              maurice@svay.Com
 
-namespace svay;
+namespace facedetect;
 
 use Exception;
-use svay\Exception\NoFaceException;
 
-class FaceDetector
-{
+class FaceDetector {
 
     protected $detection_data;
     protected $canvas;
@@ -42,39 +40,34 @@ class FaceDetector
      *
      * @throws Exception
      */
-    public function __construct($detection_data = 'detection.dat')
-    {
+    public function __construct($detection_data = 'detection.dat') {
         if (is_array($detection_data)) {
             $this->detection_data = $detection_data;
             return;
         }
-    
+
         if (!is_file($detection_data)) {
             // fallback to same file in this class's directory
             $detection_data = dirname(__FILE__) . DIRECTORY_SEPARATOR . $detection_data;
-            
+
             if (!is_file($detection_data)) {
-                throw new \Exception("Couldn't load detection data");
+                throw new Exception("Couldn't load detection data");
             }
         }
-        
+
         $this->detection_data = unserialize(file_get_contents($detection_data));
     }
 
-    public function faceDetect($file)
-    {
+    public function faceDetect($file) {
         if (is_resource($file)) {
 
             $this->canvas = $file;
-
         } elseif (is_file($file)) {
 
             $this->canvas = imagecreatefromjpeg($file);
-
         } elseif (is_string($file)) {
 
             $this->canvas = imagecreatefromstring($file);
-            
         } else {
 
             throw new Exception("Can not load $file");
@@ -92,8 +85,11 @@ class FaceDetector
             $ratio = $im_height / 240;
         }
 
+        $setWidth = floor($im_width / $ratio);
+        $setHeight = floor($im_height / $ratio);
+
         if ($ratio != 0) {
-            $this->reduced_canvas = imagecreatetruecolor($im_width / $ratio, $im_height / $ratio);
+            $this->reduced_canvas = imagecreatetruecolor($setWidth, $setHeight);
 
             imagecopyresampled(
                 $this->reduced_canvas,
@@ -102,8 +98,8 @@ class FaceDetector
                 0,
                 0,
                 0,
-                $im_width / $ratio,
-                $im_height / $ratio,
+                $setWidth,
+                $setHeight,
                 $im_width,
                 $im_height
             );
@@ -117,7 +113,7 @@ class FaceDetector
                 $stats['height']
             );
 
-            if ($this->face['w'] > 0) {
+            if (!empty($this->face['w']) && $this->face['w'] > 0) {
                 $this->face['x'] *= $ratio;
                 $this->face['y'] *= $ratio;
                 $this->face['w'] *= $ratio;
@@ -132,20 +128,19 @@ class FaceDetector
                 $stats['height']
             );
         }
-        return ($this->face['w'] > 0);
+        return (!empty($this->face['w']) && $this->face['w'] > 0);
     }
 
 
-    public function toJpeg()
-    {
+    public function toJpeg() {
         $color = imagecolorallocate($this->canvas, 255, 0, 0); //red
 
         imagerectangle(
             $this->canvas,
             $this->face['x'],
             $this->face['y'],
-            $this->face['x']+$this->face['w'],
-            $this->face['y']+ $this->face['w'],
+            $this->face['x'] + $this->face['w'],
+            $this->face['y'] + $this->face['w'],
             $color
         );
 
@@ -160,12 +155,11 @@ class FaceDetector
      *
      * @param string|null $outFileName file name to store. If null, will be printed to output
      *
-     * @throws NoFaceException
+     * @throws Exception
      */
-    public function cropFaceToJpeg($outFileName = null)
-    {
+    public function cropFaceToJpeg($outFileName = null) {
         if (empty($this->face)) {
-            throw new NoFaceException('No face detected');
+            throw new Exception('No face detected');
         }
 
         $canvas = imagecreatetruecolor($this->face['w'], $this->face['w']);
@@ -178,18 +172,15 @@ class FaceDetector
         imagejpeg($canvas, $outFileName);
     }
 
-    public function toJson()
-    {
+    public function toJson() {
         return json_encode($this->face);
     }
 
-    public function getFace()
-    {
+    public function getFace() {
         return $this->face;
     }
 
-    protected function getImgStats($canvas)
-    {
+    protected function getImgStats($canvas) {
         $image_width = imagesx($canvas);
         $image_height = imagesy($canvas);
         $iis =  $this->computeII($canvas, $image_width, $image_height);
@@ -201,59 +192,57 @@ class FaceDetector
         );
     }
 
-    protected function computeII($canvas, $image_width, $image_height)
-    {
-        $ii_w = $image_width+1;
-        $ii_h = $image_height+1;
+    protected function computeII($canvas, $image_width, $image_height) {
+        $ii_w = $image_width + 1;
+        $ii_h = $image_height + 1;
         $ii = array();
         $ii2 = array();
 
-        for ($i=0; $i<$ii_w; $i++) {
+        for ($i = 0; $i < $ii_w; $i++) {
             $ii[$i] = 0;
             $ii2[$i] = 0;
         }
 
-        for ($i=1; $i<$ii_h-1; $i++) {
-            $ii[$i*$ii_w] = 0;
-            $ii2[$i*$ii_w] = 0;
+        for ($i = 1; $i < $ii_h - 1; $i++) {
+            $ii[$i * $ii_w] = 0;
+            $ii2[$i * $ii_w] = 0;
             $rowsum = 0;
             $rowsum2 = 0;
-            for ($j=1; $j<$ii_w-1; $j++) {
+            for ($j = 1; $j < $ii_w - 1; $j++) {
                 $rgb = ImageColorAt($canvas, $j, $i);
                 $red = ($rgb >> 16) & 0xFF;
                 $green = ($rgb >> 8) & 0xFF;
                 $blue = $rgb & 0xFF;
-                $grey = (0.2989*$red + 0.587*$green + 0.114*$blue)>>0;  // this is what matlab uses
+                $grey = intval(0.2989 * $red + 0.587 * $green + 0.114 * $blue);  // this is what matlab uses
                 $rowsum += $grey;
-                $rowsum2 += $grey*$grey;
+                $rowsum2 += $grey * $grey;
 
-                $ii_above = ($i-1)*$ii_w + $j;
-                $ii_this = $i*$ii_w + $j;
+                $ii_above = ($i - 1) * $ii_w + $j;
+                $ii_this = $i * $ii_w + $j;
 
                 $ii[$ii_this] = $ii[$ii_above] + $rowsum;
                 $ii2[$ii_this] = $ii2[$ii_above] + $rowsum2;
             }
         }
-        return array('ii'=>$ii, 'ii2' => $ii2);
+        return array('ii' => $ii, 'ii2' => $ii2);
     }
 
-    protected function doDetectGreedyBigToSmall($ii, $ii2, $width, $height)
-    {
-        $s_w = $width/20.0;
-        $s_h = $height/20.0;
+    protected function doDetectGreedyBigToSmall($ii, $ii2, $width, $height) {
+        $s_w = $width / 20.0;
+        $s_h = $height / 20.0;
         $start_scale = $s_h < $s_w ? $s_h : $s_w;
         $scale_update = 1 / 1.2;
         for ($scale = $start_scale; $scale > 1; $scale *= $scale_update) {
-            $w = (20*$scale) >> 0;
+            $w = intval(20 * $scale);
             $endx = $width - $w - 1;
             $endy = $height - $w - 1;
-            $step = max($scale, 2) >> 0;
-            $inv_area = 1 / ($w*$w);
+            $step = intval(max($scale, 2));
+            $inv_area = 1 / ($w * $w);
             for ($y = 0; $y < $endy; $y += $step) {
                 for ($x = 0; $x < $endx; $x += $step) {
-                    $passed = $this->detectOnSubImage($x, $y, $scale, $ii, $ii2, $w, $width+1, $inv_area);
+                    $passed = $this->detectOnSubImage($x, $y, $scale, $ii, $ii2, $w, $width + 1, $inv_area);
                     if ($passed) {
-                        return array('x'=>$x, 'y'=>$y, 'w'=>$w);
+                        return array('x' => $x, 'y' => $y, 'w' => $w);
                     }
                 } // end x
             } // end y
@@ -261,14 +250,13 @@ class FaceDetector
         return null;
     }
 
-    protected function detectOnSubImage($x, $y, $scale, $ii, $ii2, $w, $iiw, $inv_area)
-    {
-        $mean  = ($ii[($y+$w)*$iiw + $x + $w] + $ii[$y*$iiw+$x] - $ii[($y+$w)*$iiw+$x] - $ii[$y*$iiw+$x+$w])*$inv_area;
+    protected function detectOnSubImage($x, $y, $scale, $ii, $ii2, $w, $iiw, $inv_area) {
+        $mean  = ($ii[($y + $w) * $iiw + $x + $w] + $ii[$y * $iiw + $x] - $ii[($y + $w) * $iiw + $x] - $ii[$y * $iiw + $x + $w]) * $inv_area;
 
-        $vnorm = ($ii2[($y+$w)*$iiw + $x + $w]
-                  + $ii2[$y*$iiw+$x]
-                  - $ii2[($y+$w)*$iiw+$x]
-                  - $ii2[$y*$iiw+$x+$w])*$inv_area - ($mean*$mean);
+        $vnorm = ($ii2[($y + $w) * $iiw + $x + $w]
+            + $ii2[$y * $iiw + $x]
+            - $ii2[($y + $w) * $iiw + $x]
+            - $ii2[$y * $iiw + $x + $w]) * $inv_area - ($mean * $mean);
 
         $vnorm = $vnorm > 1 ? sqrt($vnorm) : 1;
 
@@ -302,16 +290,16 @@ class FaceDetector
                     for ($i_rect = 0; $i_rect < $count_rects; $i_rect++) {
                         $s = $scale;
                         $rect = $rects[$i_rect];
-                        $rx = ($rect[0]*$s+$x)>>0;
-                        $ry = ($rect[1]*$s+$y)>>0;
-                        $rw = ($rect[2]*$s)>>0;
-                        $rh = ($rect[3]*$s)>>0;
+                        $rx = intval($rect[0] * $s + $x);
+                        $ry = intval($rect[1] * $s + $y);
+                        $rw = intval($rect[2] * $s);
+                        $rh = intval($rect[3] * $s);
                         $wt = $rect[4];
 
-                        $r_sum = ($ii[($ry+$rh)*$iiw + $rx + $rw]
-                                  + $ii[$ry*$iiw+$rx]
-                                  - $ii[($ry+$rh)*$iiw+$rx]
-                                  - $ii[$ry*$iiw+$rx+$rw])*$wt;
+                        $r_sum = ($ii[($ry + $rh) * $iiw + $rx + $rw]
+                            + $ii[$ry * $iiw + $rx]
+                            - $ii[($ry + $rh) * $iiw + $rx]
+                            - $ii[$ry * $iiw + $rx + $rw]) * $wt;
 
                         $rect_sum += $r_sum;
                     }
@@ -320,24 +308,20 @@ class FaceDetector
 
                     $current_node = null;
 
-                    if ($rect_sum >= $node_thresh*$vnorm) {
+                    if ($rect_sum >= $node_thresh * $vnorm) {
 
                         if ($rightidx == -1) {
 
                             $tree_sum = $rightval;
-
                         } else {
 
                             $current_node = $tree[$rightidx];
-
                         }
-
                     } else {
 
                         if ($leftidx == -1) {
 
                             $tree_sum = $leftval;
-
                         } else {
 
                             $current_node = $tree[$leftidx];
